548e090c378a0e2dcf67cfbd4ccc463f297d89c0
nknysh <nadimarkava@gmail.com>
2024-05-31 14:49:50 +0300

    added check out complete page

---
+++ src/main/java/common/CheckOutCompleteBasePage.java
@@ -0,0 +1,14 @@
+package common;
+
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
+import com.zebrunner.carina.webdriver.decorator.PageOpeningStrategy;
+import com.zebrunner.carina.webdriver.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public abstract class CheckOutCompleteBasePage extends AbstractPage {
+
+    public CheckOutCompleteBasePage(WebDriver driver) {
+        super(driver);
+    }
+}
