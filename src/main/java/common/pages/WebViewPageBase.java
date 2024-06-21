5ac14b96b4db51618a6fc6d2e79545e16e7446fe
nknysh <nadimarkava@gmail.com>
2024-06-21 11:41:31 +0300

    added test for Web view (Android)

---
+++ src/main/java/common/pages/WebViewPageBase.java
@@ -0,0 +1,11 @@
+package common.pages;
+
+import com.zebrunner.carina.webdriver.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+
+public class WebViewPageBase extends AbstractPage {
+
+    protected WebViewPageBase(WebDriver driver) {
+        super(driver);
+    }
+}
