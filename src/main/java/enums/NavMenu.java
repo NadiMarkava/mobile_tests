32f92cdc9152dafa6ed49389be259b14c3f81f96
nknysh <nadimarkava@gmail.com>
2024-06-24 12:25:31 +0300

    resolved conflict

--- src/main/java/enums/NavMenu.java
+++ src/main/java/enums/NavMenu.java
@@ -1,27 +1,13 @@
 package enums;
-
-<<<<<<< HEAD
-import common.pages.LoginPageBase;
-import common.pages.ProductsPageBase;
-import common.pages.SwagLabsAbstractPageBase;
-=======
 import common.pages.*;
->>>>>>> parent of 09dca47 (reversed)
 
 public enum NavMenu {
 
     ALL_ITEMS("test-ALL ITEMS", ProductsPageBase.class),
 
-<<<<<<< HEAD
-    LOG_OUT("test-LOGOUT", LoginPageBase.class);
-=======
     LOG_OUT("test-LOGOUT", LoginPageBase.class),
 
-    DRAWING("test-DRAWING", DrawingPageBase.class),
-
     WEB_VIEW("test-WEBVIEW", WebViewSelectionPageBase.class);
->>>>>>> parent of 09dca47 (reversed)
-
 
     private final String label;
     private final Class<? extends SwagLabsAbstractPageBase> basePage;
